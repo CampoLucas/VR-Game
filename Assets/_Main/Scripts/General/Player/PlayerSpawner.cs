fca72c3d33c080c8412ad16f93bcb4ed93e17c5d
FedeLeguizamo <128617595+FedeLeguizamo@users.noreply.github.com>
2026-06-20 19:04:16 -0300

    Spawn Fix

--- Assets/_Main/Scripts/General/Player/PlayerSpawner.cs
+++ Assets/_Main/Scripts/General/Player/PlayerSpawner.cs
@@ -1,22 +1,54 @@
+using System;
+using Oculus.Interaction.Locomotion;
 using UnityEngine;
+using UnityEngine.Serialization;
+using CharacterController = Oculus.Interaction.Locomotion.CharacterController;
 
 namespace VRGame.Player
 {
     public class PlayerSpawner : MonoBehaviour
     {
-        [SerializeField] private Transform player;
-        [SerializeField] private Transform playerSpawnAnchor;
+        [SerializeField] private FirstPersonLocomotor locomotor;
+        [SerializeField] private Transform targetAnchor;
+        
+        
+        [SerializeField] private OVRCameraRig cameraRig;
+        [SerializeField] private CharacterController playerController;
 
         private void Awake()
         {
-            if (player == null || playerSpawnAnchor == null)
+            if (!locomotor)
             {
-                Debug.LogError($"[{nameof(PlayerSpawner)}] player or playerSpawnAnchor not assigned.", this);
-                return;
+                locomotor = FindObjectOfType<FirstPersonLocomotor>();
             }
 
-            player.position = playerSpawnAnchor.position;
-            player.rotation = playerSpawnAnchor.rotation;
+            Spawn();
+        }
+
+        private void OnValidate()
+        {
+            if (!locomotor)
+            {
+                Debug.LogWarning($"[{nameof(PlayerSpawner)}] WARNING: The FirstPersonLocomotor reference is null, it will use FindObjectOfType in the awake");
+            }
+        }
+
+        [ContextMenu("Spawn")]
+        private void Spawn()
+        {
+            Spawn(targetAnchor);
+        }
+
+        private void Spawn(Transform target)
+        {
+            // 1. RECENTRAR EL HEADSET: Borra el offset del guardián acumulado por el movimiento físico en la vida real
+            if (OVRManager.display != null)
+            {
+                OVRManager.display.RecenterPose();
+            }
+            var pose = new Pose(target.position, target.rotation);
+            var locomotionEvent = new LocomotionEvent(0, pose, LocomotionEvent.TranslationType.Absolute, LocomotionEvent.RotationType.Absolute);
+            locomotor.HandleLocomotionEvent(locomotionEvent);
         }
     }
 }
