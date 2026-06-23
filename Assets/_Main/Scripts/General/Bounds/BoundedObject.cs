73e66909121002510f5edd7ae5c0beac750c97a9
Lucas Campo <102192546+CampoLucas@users.noreply.github.com>
2026-06-23 13:14:22 -0300

    The broken lever repositions when is out of bounds

--- Assets/_Main/Scripts/General/Bounds/BoundedObject.cs
+++ Assets/_Main/Scripts/General/Bounds/BoundedObject.cs
@@ -1,67 +1,27 @@
 using System;
 using Oculus.Interaction;
 using UnityEngine;
-using VRGame.General.Interactables;
 
-namespace VRGame.Puzzles
+namespace VRGame.General.Bounds
 {
-    public class Gear : MonoBehaviour, IBoundedObject
+    public class BoundedObject : MonoBehaviour, IBoundedObject
     {
-
-        public SnapInteractor Interactor => interactor;
-
         [Header("References")]
-        [SerializeField] private Transform parent;
-
         [SerializeField] private Grabbable grabbable;
         [SerializeField] private GameObject interactables;
-        [SerializeField] private SnapInteractor interactor;
         [SerializeField] private Rigidbody rb;
-
+        
         private Transform _transform;
         private Vector3 _startPos;
         private Quaternion _startRot;
 
-
         private void Awake()
         {
             _transform = transform;
-            if (!grabbable) grabbable = GetComponent<Grabbable>();
-            if (!rb) rb = GetComponent<Rigidbody>();
-
-            _transform.SetParent(parent);
-
             _startPos = _transform.position;
             _startRot = _transform.rotation;
         }
-
-        public void SnapOn(SnapParams gearSnapPoint)
-        {
-            // Disable the grababble
-            if (gearSnapPoint.DisableGrabbable)
-            {
-                grabbable.enabled = false;
-                interactables.SetActive(false);
-            }
-
-            // Set kinematic
-            rb.isKinematic = true;
-
-            _transform.position = gearSnapPoint.Position;
-            _transform.rotation = gearSnapPoint.Rotation;
-
-            _transform.SetParent(gearSnapPoint.Parent);
-        }
-
-        public void SnapOff()
-        {
-            grabbable.enabled = true;
-            interactables.SetActive(true);
-
-
-            _transform.SetParent(parent);
-        }
-
+        
         public void Reposition()
         {
             // Disable interactables
@@ -84,6 +44,13 @@ namespace VRGame.Puzzles
             // Enable interactables
             grabbable.enabled = true;
             interactables.SetActive(true);
+        }
+
+        private void OnDestroy()
+        {
+            grabbable = null;
+            interactables = null;
+            rb = null;
         }
     }
 }
